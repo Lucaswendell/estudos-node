bc2fe656be1202b2d4e5cbce36beb411b43535b7
Lucaswendell <wendellucas303@gmail.com>
2022-08-21 22:56:14 -0300

    Core Modules:  Informações do arquivo

---
+++ 3_CORE_MODULES/11_detalhes_arquivos/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+fs.stat("pasta", (err, stats) => {
+  if (err) {
+    console.log(err.message);
+    return;
+  }
+
+  console.log(stats.isFile());
+  console.log(stats.isDirectory());
+  console.log(stats.isSymbolicLink());
+  console.log(stats.ctime);
+  console.log(stats.size);
+});
