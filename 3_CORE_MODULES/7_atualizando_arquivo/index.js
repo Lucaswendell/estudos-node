098d80fea7837f30adb4d6ffcba22064f9a6728a
Lucaswendell <wendellucas303@gmail.com>
2022-08-21 22:20:55 -0300

    Core Modules: Atualizando arquivo com fs

---
+++ 3_CORE_MODULES/7_atualizando_arquivo/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const urlInfo = require("url").parse(req.url, true);
+  const { name } = urlInfo.query;
+  if (!name) {
+    fs.readFile("index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  } else {
+    const nameNewLine = `${name},\r\n`;
+    fs.appendFile("arquivo.txt", nameNewLine, (err, data) => {
+      res.writeHead(302, {
+        Location: "/",
+      });
+      return res.end();
+    });
+  }
+});
+
+server.listen(port, () => {
+  console.log(`rodando na porta ${port}`);
+});
