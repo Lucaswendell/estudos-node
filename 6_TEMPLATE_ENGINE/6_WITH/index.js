95c641dbe55b6c5fd3912eb507c98876c2ef0912
Lucaswendell <wendellucas303@gmail.com>
2022-10-01 18:05:07 -0300

    Template Engine: With

---
+++ 6_TEMPLATE_ENGINE/6_WITH/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Lucas",
+    surname: "Wendel",
+  };
+
+  const auth = false;
+  const approved = false;
+
+  res.render("home", { user, auth, approved });
+});
+
+app.get("/post", (req, res) => {
+  const post = {
+    title: "Aprender Node.js",
+    category: "Javascript",
+    body: "Artigo",
+    commet: 4,
+  };
+
+  res.render("blogpost", { post });
+});
+
+app.get("/dashboard", (req, res) => {
+  const items = ["Item a", "Item B", "Item C"];
+
+  res.render("dashboard", { items });
+});
+
+app.listen(3000, () => {
+  console.log("Server rodando na porta 3000");
+});
