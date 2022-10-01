2c208f025f6e1dfdf3fbfbb8b4d154154eb2792f
Lucaswendell <wendellucas303@gmail.com>
2022-10-01 17:48:43 -0300

    Template Engine: Condicionais

---
+++ 6_TEMPLATE_ENGINE/4_CONDICIONAIS/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Lucas",
+    surname: "Wendel",
+  };
+
+  const auth = false;
+  const approved = false;
+
+  res.render("home", { user, auth, approved });
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+app.listen(3000, () => {
+  console.log("Server rodando na porta 3000");
+});
